19bb7fff0ea25ef6e37b58eb518acb9681ac6cf2
Sebastián Mateo Espinosa <matthiuw005@gmail.com>
2026-06-08 15:54:56 -0500

    Add: Nuevo modelo

---
+++ src/models/Sprint.js
@@ -0,0 +1,46 @@
+import { DataTypes } from 'sequelize';
+
+export default function (sequelize) {
+  const Sprint = sequelize.define('sprints', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    goal: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  }, {
+    timestamps: true
+  });
+
+  Sprint.associate = function (models) {
+    Sprint.belongsTo(models.Project, { 
+      foreignKey: 'projectId', 
+      targetId: 'id' 
+    });
+    
+    Sprint.hasMany(models.UserStory, { 
+      foreignKey: 'sprintId', 
+      sourceKey: 'id' 
+    });
+  };
+
+  return Sprint;
+}
